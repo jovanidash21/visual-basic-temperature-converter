e77c1abb7a9af7700c131317dc7eedefc5b9b3c6
jovanidash21 <jwardash21@gmail.com>
2017-01-12 20:13:35 +0800

    Add Function

--- TemperatureConverter/Module1.vb
+++ TemperatureConverter/Module1.vb
@@ -1,7 +1,45 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Console.WriteLine("What do you want to convert?")
+        Console.WriteLine("a.) Celcius to Fahrenheit")
+        Console.WriteLine("b.) Fahrenheit to Celcius")
+        Console.Write("Enter the letter: ")
+        Dim input As String = Console.ReadLine()
+        If (input = "a" Or input = "A") Then
+            Console.Write("What is the value?: ")
+            Dim inputCelcius As String = Console.ReadLine()
+            Dim celcius As Double
+            If Double.TryParse(inputCelcius, celcius) Then
+                Dim fahrenheit As String = celcius * (9 / 5) + 32
+                Console.WriteLine("The value in fahrenheit is: " + fahrenheit)
+                Console.WriteLine()
+                Main()
+            Else
+                Console.WriteLine("Invalid Input")
+                Console.WriteLine()
+                Main()
+            End If
+        ElseIf (input = "b" Or input = "B") Then
+            Console.Write("What is the value?: ")
+            Dim inputFahrenheit As String = Console.ReadLine()
+            Dim fahrenheit As Double
+            If Double.TryParse(inputFahrenheit, fahrenheit) Then
+                Dim celcius As String = (fahrenheit - 32) * (5 / 9)
+                Console.WriteLine("The value in celcius is: " + celcius)
+                Console.WriteLine()
+                Main()
+            Else
+                Console.WriteLine("Invalid Input")
+                Console.WriteLine()
+                Main()
+            End If
+        Else
+            Console.WriteLine("Invalid Input")
+            Console.WriteLine()
+            Main()
+        End If
+        Console.ReadLine()
     End Sub
 
 End Module
